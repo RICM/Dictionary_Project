166615eee88473b978f0d57a00175e22adb97b4d
mnogueron <matthieu.nogueron@live.fr>
2015-04-07 16:56:51 +0200

    add dictionnaire.c

--- src/dictionnire.c
+++ src/dictionnire.c
@@ -4,8 +4,8 @@ void insertion_dictionnaire(pDictionnaire d, char *mot, int l, int c){
 	pDictionnaire tmpSucc = d->succ;
 	pDictionnaire tmpPred = d;
 	int bEnd = 0;
-	while(tmp != NULL && !bEnd){
-
+	while(tmpPred != NULL && !bEnd){
+		if()
 	}
 	if(tmp == NULL)
 
